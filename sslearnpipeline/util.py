f4f10c2c1ea9682476d720fa58ce023f98a8c47a
Mihir Mongia <mmongia@pslogin7c.slac.stanford.edu>
2017-06-09 15:25:49 -0700

    utils

--- sslearnpipeline/util.py
+++ sslearnpipeline/util.py
@@ -14,11 +14,18 @@ def create_jpeg(img, output_filename):
     mn = np.min(img)
     mx = np.max(img)
     img = img - mn
-    img /= (mx-mn)
+    img = img*(1.0)/((mx-mn)*(1.0))
     img = np.maximum(0.0, np.minimum(1.0, img))
-    img = jet(img)[:,:,0:3]
+    img = jet(img[0,:,:,0])[:,:,0:3]
+    print(img.shape)
+    #raw_input("see above")
     img *= 255.0
     img = np.uint8(img)
+    print(img)
+    print(img.shape)
+    print(img.dtype)
+    print(img.shape)
+    #raw_input("what do we see?")
     img = Image.fromarray(img)
     img.save(output_filename)
     print("saved %s" % output_filename)
@@ -31,25 +38,25 @@ def is_closed_five_point_box(points):
 
 def prep_img_for_vgg16(img, mean_to_subtract=None, interp='lanczos'):
     # resize options are here: https://docs.scipy.org/doc/scipy/reference/generated/scipy.misc.imresize.html
-    dest = np.empty((224,224,3), dtype=np.float32)
+    #dest = np.empty((224,224,3), dtype=np.float32)
+    dest = img
+    #if img.dtype != np.float32:
+    #    img = img.astype(np.float32)
 
-    if img.dtype != np.float32:
-        img = img.astype(np.float32)
+    #assert len(img.shape)==2
 
-    assert len(img.shape)==2
-
-    if img.shape != (224, 224):
-        img = imresize(img, (224,224), interp=interp, mode='F')
+    #if img.shape != (224, 224):
+    #    img = imresize(img, (224,224), interp=interp, mode='F')
 
     orig_resized_mean = np.mean(img)
 
-    if mean_to_subtract is None:
-        mean_to_subtract = orig_resized_mean
+    #if mean_to_subtract is None:
+    #    mean_to_subtract = orig_resized_mean
 
-    img -= mean_to_subtract
+    #img -= mean_to_subtract
 
-    for ch in range(3):
-        dest[:,:,ch]=img[:,:]
+    #for ch in range(3):
+    #    dest[:,:,ch]=img[:,:]
         
-    dest = np.expand_dims(dest, axis=0)
+    #dest = np.expand_dims(dest, axis=0)
     return dest, orig_resized_mean
